bd8b283f75da7d4fa5b50c3a04c51ec96d076f13
Thomas Spycher <tsthomas123@gmail.com>
2017-02-08 11:23:37 -0800

    initial commit

---
+++ java.js
@@ -0,0 +1,27 @@
+function skiResort(difficulty, price, terrainPark, conditions) {
+    this.difficulty = difficulty;
+    this.price = price;
+    this.conditions = conditions;
+    this.terrainPark = terrainPark;
+}
+
+
+
+function skier(level, priceLimit, terrainPark) {
+    this.level = level;
+    this.priceLimit = priceLimit;
+}
+
+
+
+var squaw = new skiResort("everyone",124,"some", );
+var sugarBowl = new skiResort("intermediate",109,"some", );
+var mtRose = new skiResort("everyone",115,"some", );
+var heavenly = new skiResort("everyone",135,"yes", );
+var northstar = new skiResort("begginer",140,"yes", );
+var homewood = new skiResort("begginer",80,"some", );
+var boreal = new skiResort("begginer",74,"yes", );
+var diamondPeak = new skiResort("intermediate",74,"some", );
+var kirkwood = new skiResort("everyone",107,"some", );
+
+
