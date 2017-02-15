8ca0e0b288e8e582b85d5cc09ade9e272948d35f
Thomas Spycher <tsthomas123@gmail.com>
2017-02-15 11:15:41 -0800

    version 2

--- java.js
+++ java.js
@@ -1,7 +1,8 @@
-function skiResort(difficulty, price, terrainPark, conditions) {
+function skiResort(difficulty, price, terrainPark, base, freshSnow) {
     this.difficulty = difficulty;
     this.price = price;
-    this.conditions = conditions;
+    this.base = base;
+    this.freshSnow = freshSnow;
     this.terrainPark = terrainPark;
 }
 
@@ -14,14 +15,33 @@ function skier(level, priceLimit, terrainPark) {
 
 
 
-var squaw = new skiResort("everyone",124,"some", );
-var sugarBowl = new skiResort("intermediate",109,"some", );
-var mtRose = new skiResort("everyone",115,"some", );
-var heavenly = new skiResort("everyone",135,"yes", );
-var northstar = new skiResort("begginer",140,"yes", );
-var homewood = new skiResort("begginer",80,"some", );
-var boreal = new skiResort("begginer",74,"yes", );
-var diamondPeak = new skiResort("intermediate",74,"some", );
-var kirkwood = new skiResort("everyone",107,"some", );
+// var squaw = new skiResort("everyone",124,"some", , );
+// var sugarBowl = new skiResort("intermediate",109,"some", , );
+// var mtRose = new skiResort("everyone",115,"some", , );
+// var heavenly = new skiResort("everyone",135,"yes", , );
+// var northstar = new skiResort("begginer",140,"yes", , );
+// var homewood = new skiResort("begginer",80,"some", , );
+// var boreal = new skiResort("begginer",74,"yes", , );
+// var diamondPeak = new skiResort("intermediate",74,"some", , );
+// var kirkwood = new skiResort("everyone",107,"some", , );
 
 
+function api() {
+    $.ajax({
+        url:"http://api.worldweatheronline.com/premium/v1/ski.ashx?key=28897923be88464da70190435171402&q=Truckee&format=json&includelocation=yes",
+        type:'GET',
+        crossDomain: true,
+        dataType: 'jsonp',
+        success: function (result) {
+            console.log(result);
+            getData(result)
+        },
+        error: function () {
+            alert('Failed!');
+        }
+    });
+}
+
+function getData(result) {
+    console.log(result.data.request);
+}
