ddd70e7efe96dc3c867ac8291475868e9e39c4e7
Thomas Spycher <tsthomas123@gmail.com>
2017-02-24 11:38:24 -0800

    version 4

--- java.js
+++ java.js
@@ -1,4 +1,5 @@
-function skiResort(difficulty, price, terrainPark, freshSnow) {
+function skiResort(name, difficulty, price, terrainPark, freshSnow) {
+    this.name = name;
     this.difficulty = difficulty;
     this.price = price;
     this.freshSnow = freshSnow;
@@ -12,9 +13,48 @@ function skier(level, priceLimit, terrainPark) {
 }
 
 
-function api() {
+// 1 = beginner skier/resort
+// 2 = intermediate skier/resort
+// 3 = advanced skier/resort
+// 0 = resort for all levels
+
+// 1 = no terrain park/never use it
+// 2 = some terrain park/will use it
+// 3 = a lot of terrain park/mainly ride the terrain park
+
+var alpineMeadows = new skiResort("Alpine Meadows",0,124,1,0);
+var squaw = new skiResort("Squaw",0,124,1,0);
+var sugarBowl = new skiResort("Sugar bowl",2,109,1,0);
+var mtRose = new skiResort("Mount Rose",0,115,1,0);
+var heavenly = new skiResort("Heavenly",0,135,2,0);
+var northstar = new skiResort("Northstar",1,140,2,0);
+var homewood = new skiResort("Homewood",1,80,1,0);
+var boreal = new skiResort("Boreal",1,74,2,0);
+var diamondPeak = new skiResort("Diamond Peak",2,74,1,0);
+var kirkwood = new skiResort("Kirkwood",0,107,1,0);
+
+var resorts = [squaw, sugarBowl, mtRose, heavenly, northstar, homewood, boreal, diamondPeak, kirkwood];
+
+
+api();
+
+//"alpine Meadows"  -> Alpine Meadows
+//"alpine Meadows"  -> Squaw
+//"incline village" -> Mt Rose
+//"zephyr cove"     -> Homewood
+//"kit carson"      -> Kirkwood
+//"carnelian bay"   -> Northstar
+//"                 -> Heavenly
+//"                 -> Diamond Peak
+//"                 -> Sugar Bowl
+
+
+
+var theSkier = new skier(3,100,1);
+
+function api(city) {
     $.ajax({
-        url:"http://api.worldweatheronline.com/premium/v1/ski.ashx?key=28897923be88464da70190435171402&q=Truckee&format=json&includelocation=yes",
+        url:"http://api.worldweatheronline.com/premium/v1/ski.ashx?key=28897923be88464da70190435171402&format=json&includelocation=yes&q=" + city,
         type:'GET',
         crossDomain: true,
         dataType: 'jsonp',
@@ -29,41 +69,28 @@ function api() {
 }
 
 function getData(result) {
-    for (var i = 0; i < result.data.weather.length; i++) {
-        console.log(result.data.weather[i].totalSnowfall_cm);
-    }
-
+        console.log(result.data.nearest_area[0].areaName[0].value);
 }
-
-
-
-var squaw = new skiResort("everyone",124,"some",0);
-var sugarBowl = new skiResort("intermediate",109,"some",0);
-var mtRose = new skiResort("everyone",115,"some",0);
-var heavenly = new skiResort("everyone",135,"yes",0);
-var northstar = new skiResort("begginer",140,"yes",0);
-var homewood = new skiResort("begginer",80,"some",0);
-var boreal = new skiResort("begginer",74,"yes",0);
-var diamondPeak = new skiResort("intermediate",74,"some",0);
-var kirkwood = new skiResort("everyone",107,"some",0);
-
-var theSkier = new skier("advanced",100,"yes");
 
 
 
 
 function pickResort() {
+    console.log(theSkier);
 
-    if(theSkier.priceLimit >= skiResort.price) {
+    for (var i=0; i<resorts.length; i++) {
+        console.log(resorts[i]);
 
-        if ((theSkier.level === skiResort.difficulty) || (skiResort.difficulty === "everyone")) {
+        if (theSkier.priceLimit >= resorts[i].price) {
 
-            if(theSkier.terrainPark <= skiResort.terrainPark) {
+            if ((theSkier.level === resorts[i].difficulty) || (resorts[i].difficulty === 0)) {
+                console.log("hello");
 
-                //rank in order of fresh snow fall
 
+                if (theSkier.terrainPark <= resorts[i].terrainPark) {
+                    //rank in order of fresh snow fall
+                }
             }
         }
     }
-    //print all ski resorts
 }
