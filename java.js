a9a8f80b5a92584864e016dac699191846c8090e
Thomas Spycher <tsthomas123@gmail.com>
2017-03-01 11:31:35 -0800

    version 5

--- java.js
+++ java.js
@@ -1,12 +1,13 @@
-function skiResort(name, difficulty, price, terrainPark, freshSnow) {
+function SkiResort(name, coordinates, difficulty, price, terrainPark, freshSnow) {
     this.name = name;
+    this.coordinates = coordinates;
     this.difficulty = difficulty;
     this.price = price;
-    this.freshSnow = freshSnow;
     this.terrainPark = terrainPark;
+    this.freshSnow = freshSnow;
 }
 
-function skier(level, priceLimit, terrainPark) {
+function Skier(level, priceLimit, terrainPark) {
     this.level = level;
     this.priceLimit = priceLimit;
     this.terrainPark = terrainPark;
@@ -22,70 +23,52 @@ function skier(level, priceLimit, terrainPark) {
 // 2 = some terrain park/will use it
 // 3 = a lot of terrain park/mainly ride the terrain park
 
-var alpineMeadows = new skiResort("Alpine Meadows",0,124,1,0);
-var squaw = new skiResort("Squaw",0,124,1,0);
-var sugarBowl = new skiResort("Sugar bowl",2,109,1,0);
-var mtRose = new skiResort("Mount Rose",0,115,1,0);
-var heavenly = new skiResort("Heavenly",0,135,2,0);
-var northstar = new skiResort("Northstar",1,140,2,0);
-var homewood = new skiResort("Homewood",1,80,1,0);
-var boreal = new skiResort("Boreal",1,74,2,0);
-var diamondPeak = new skiResort("Diamond Peak",2,74,1,0);
-var kirkwood = new skiResort("Kirkwood",0,107,1,0);
+var alpineMeadows = new SkiResort("Alpine Meadows", "39.153309, -120.236276", 0,124,1,0);
+var squaw = new SkiResort("Squaw", "39.153309, -120.236276", 0,124,1,0);
+var mtRose = new SkiResort("Mount Rose", "39.320654, -119.884450", 0,115,1,0);
+var northstar = new SkiResort("Northstar", "39.257063, -120.132625", 1,140,2,0);
+var homewood = new SkiResort("Homewood", "39.080040, -120.169578", 1,80,1,0);
+var boreal = new SkiResort("Boreal", "39.332796, -120.349905", 1,74,2,0);
+var kirkwood = new SkiResort("Kirkwood", "38.677414, -120.069687", 0,107,1,0);
 
-var resorts = [squaw, sugarBowl, mtRose, heavenly, northstar, homewood, boreal, diamondPeak, kirkwood];
+var resorts = [alpineMeadows, squaw, mtRose, northstar, homewood, boreal, kirkwood];
 
-
-api();
-
-//"alpine Meadows"  -> Alpine Meadows
-//"alpine Meadows"  -> Squaw
-//"incline village" -> Mt Rose
-//"zephyr cove"     -> Homewood
-//"kit carson"      -> Kirkwood
-//"carnelian bay"   -> Northstar
-//"                 -> Heavenly
-//"                 -> Diamond Peak
-//"                 -> Sugar Bowl
+var theSkier = new Skier(3,100,1);
 
 
+for(var i=0; i<resorts.length; i++) {
+    api(resorts[i]);
+}
 
-var theSkier = new skier(3,100,1);
-
-function api(city) {
+function api(resort) {
     $.ajax({
-        url:"http://api.worldweatheronline.com/premium/v1/ski.ashx?key=28897923be88464da70190435171402&format=json&includelocation=yes&q=" + city,
+        url:"http://api.worldweatheronline.com/premium/v1/ski.ashx?key=28897923be88464da70190435171402&q=" + resort.coordinates + "&format=json&includelocation=yes",
         type:'GET',
         crossDomain: true,
         dataType: 'jsonp',
         success: function (result) {
-            console.log(result);
-            getData(result)
+
+            resort.freshSnow = result.data.weather[5].totalSnowfall_cm;
+            console.log(resort.freshSnow);
+
         },
-        error: function () {
+        error:   function () {
             alert('Failed!');
         }
     });
 }
 
-function getData(result) {
-        console.log(result.data.nearest_area[0].areaName[0].value);
-}
 
 
 
 
 function pickResort() {
-    console.log(theSkier);
-
     for (var i=0; i<resorts.length; i++) {
-        console.log(resorts[i]);
 
         if (theSkier.priceLimit >= resorts[i].price) {
 
-            if ((theSkier.level === resorts[i].difficulty) || (resorts[i].difficulty === 0)) {
-                console.log("hello");
-
+            if ((theSkier.level >= resorts[i].difficulty) || (resorts[i].difficulty === 0)) {
+                console.log(resorts[i]);
 
                 if (theSkier.terrainPark <= resorts[i].terrainPark) {
                     //rank in order of fresh snow fall
